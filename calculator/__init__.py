24490965679b5043c82f447f642e4fa63a8c433a
Pedro Silva <pedro.ribeiro.silva@tecnico.ulisboa.pt>
2025-08-28 21:13:53 +0100

    chore: set package __version__ = 1.0.0 for release v1.0.0

---
+++ calculator/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for python-calculator."""
+
+__all__ = ["add", "subtract", "multiply", "divide"]
+__version__ = "1.0.0"
+
+from .operations import add, divide, multiply, subtract
